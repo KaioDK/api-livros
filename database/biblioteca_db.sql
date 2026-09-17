e8924fa6c3bf881396658dd09af08561354b088c
KaioDK <kaio.lima6924@gmail.com>
2026-09-17 07:49:24 -0300

    banco

--- database/biblioteca_db.sql
+++ database/biblioteca_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Set-2026 às 12:43
+-- Tempo de geração: 17-Set-2026 às 12:48
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -42,7 +42,8 @@ CREATE TABLE `livros` (
 --
 
 INSERT INTO `livros` (`id`, `titulo`, `autor`, `ano_publicacao`, `disponivel`) VALUES
-(1, 'O Hobbit', 'J. R. R. Tolkien', 1937, 1);
+(1, 'O Hobbit', 'J. R. R. Tolkien', 1937, 1),
+(7, 'teste', 'adasdsa', 2054, 1);
 
 --
 -- Índices para tabelas despejadas
@@ -63,7 +64,7 @@ ALTER TABLE `livros`
 -- AUTO_INCREMENT de tabela `livros`
 --
 ALTER TABLE `livros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
